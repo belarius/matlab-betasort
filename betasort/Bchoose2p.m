1ef1221e96f455eacd36630fcf654ffe52dcdcd0
belarius <belarius@gmail.com>
2015-03-20 14:57:23 -0400

    Create Bchoose2p.m

---
+++ betasort/Bchoose2p.m
@@ -0,0 +1,18 @@
+function p = Bchoose2p(B,ch,nc,noise)
+%BCHOOSE2P Probability in a two alternative choice from N alternative data using betasort
+%   Detailed explanation goes here
+
+if isnan(B(ch,1))
+	B(ch,:) = 0;
+end
+
+if isnan(B(nc,1))
+	B(nc,:) = 0;
+end
+
+fun = @(x,v) beta_pdf(x,v(1)+1,v(2)+1).*beta_cdf(x,v(3)+1,v(4)+1);
+p = integral(@(x)fun(x,[B(ch,1) B(ch,2) B(nc,1) B(nc,2)]),0,1);
+
+p = (1-noise).*p + noise.*(0.5);
+
+end
