61c9e53ba8c5201b0cc801aeeb8a02a3e122c7b0
belarius <belarius@gmail.com>
2015-03-20 14:56:25 -0400

    Update Bupdate.m
    Clearer variable names

--- betasort/Bupdate.m
+++ betasort/Bupdate.m
@@ -1,17 +1,17 @@
-function [Bout, Rout] = Bupdate(B,R,c,calt,r,remem)
+function [Bout, Rout] = Bupdate(B,R,ch,nc,r,remem)
 %BUPDATE Update betasort memory array following feedback
 %   Detailed explanation goes here
 
-c = c(:);
-calt = calt(:);
+ch = ch(:);
+nc = nc(:);
 
 % Relax rate representations
-R([c;calt],:) = R([c;calt],:).*remem;
+R([ch;nc],:) = R([ch;nc],:).*remem;
 E = R(:,1)./sum(R,2);
 remem_R = repmat(E./(E+1) + 0.5,1,2);
 
 % Relax position representations
-nandex = intersect(find(isnan(B(:,1))),[c;calt]);
+nandex = intersect(find(isnan(B(:,1))),[ch;nc]);
 B(nandex,:) = 0;
 B = B.*remem_R.*remem;
 V = B(:,1)./sum(B,2);
@@ -19,20 +19,20 @@ V(isnan(V)) = 0.5;
 
 if r == 1 % When reward is delivered...
 	% Increase reward rate for trial stimuli
-	R([c;calt],1) = R([c;calt],1)+1;
+	R([ch;nc],1) = R([ch;nc],1)+1;
 	% Consolidate stimulus position for both trial and implicit stimuli
 	B = B + [V 1-V];
 else % Othwerwise...
 	% Decrease reward rate for trial stimuli
-	R([c;calt],2) = R([c;calt],2)+1;
+	R([ch;nc],2) = R([ch;nc],2)+1;
 	% Move trial stimuli positions apart
-	B(c,:) = B(c,:) + [0 1];
-	B(calt,:) = B(calt,:) + [1 0];
+	B(ch,:) = B(ch,:) + [0 1];
+	B(nc,:) = B(nc,:) + [1 0];
 	% Shift all implicit stimuli relative to the upper and lower boundaries
 	% set by the expected values of the trial stimuli.
-	nonc = setxor((1:size(B,1))',[c;calt]);
-	bot_post = min(V(c));
-	top_post = max(V(calt));
+	nonc = setxor((1:size(B,1))',[ch;nc]);
+	bot_post = min(V(ch));
+	top_post = max(V(nc));
 	for i = 1:length(nonc)
 		j = nonc(i);
 		if V(j) > bot_post && V(j) < top_post
@@ -53,4 +53,3 @@ Rout = R;
 Bout = B;
 
 end
-
